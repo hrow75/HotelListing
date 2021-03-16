25ab27352e8613413803a28c44250f9dd102a26c
Holly Row <hrow75@gmail.com>
2021-03-16 12:10:17 -0500

    Added Database, AutoMapper, Unit of Work and DTOs

---
+++ HotelListing/Migrations/20210311212422_SeedingData.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class SeedingData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hotels_Countries_CountyId",
+                table: "Hotels");
+
+            migrationBuilder.RenameColumn(
+                name: "CountyId",
+                table: "Hotels",
+                newName: "CountryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Hotels_CountyId",
+                table: "Hotels",
+                newName: "IX_Hotels_CountryId");
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 1, "Jamaica", "JM" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 2, "Bahamas", "BS" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 3, "Cayman Island", "CI" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 1, "Negril", 1, "Sandals Resort and Spa", 4.5 });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 2, "Nessua", 2, "Grand Palldium", 4.0 });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 3, "George Town", 3, "Comfort Suites", 4.2999999999999998 });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hotels_Countries_CountryId",
+                table: "Hotels",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hotels_Countries_CountryId",
+                table: "Hotels");
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "CountryId",
+                table: "Hotels",
+                newName: "CountyId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Hotels_CountryId",
+                table: "Hotels",
+                newName: "IX_Hotels_CountyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hotels_Countries_CountyId",
+                table: "Hotels",
+                column: "CountyId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
