6b0cccbee32e46484727f566c0eaafd4051f2d5c
Holly Row <hrow75@gmail.com>
2021-03-17 15:21:05 -0500

    Updating some code

--- HotelListing/Controllers/CountryController.cs
+++ HotelListing/Controllers/CountryController.cs
@@ -1,4 +1,6 @@
-﻿using HotelListing.IRepository;
+﻿using AutoMapper;
+using HotelListing.IRepository;
+using HotelListing.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -14,24 +16,44 @@ namespace HotelListing.Controllers
     public class CountryController : ControllerBase
     {
         private readonly IUnitofWork _unitofWork;
-        private readonly ILogger _logger;
+        private readonly ILogger <CountryController> _logger;
+        private readonly IMapper _mapper;
 
-        public CountryController(IUnitofWork unitofWork, ILogger logger)
+        public CountryController(IUnitofWork unitofWork, ILogger<CountryController> logger,
+            IMapper mapper)
         {
             _unitofWork = unitofWork;
             _logger = logger;
+            _mapper = mapper;
         }
         [HttpGet]
         public async Task<IActionResult> GetCountries()
         {
             try
             {
-                var countries = _unitofWork.Countries.GetAll();
-                return Ok(countries);
+                var countries = await _unitofWork.Countries.GetAll();
+                var results = _mapper.Map<IList<CountryDTO>>(countries);
+                return Ok(results);
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, $"Something Went Wrong in the {nameof(GetCountries)}");
+                return StatusCode(500, "Internal Server Error. Please Try Again Later.");
+            }
+        }
+
+        [HttpGet("{id/int}")]
+        public async Task<IActionResult> GetCountry(int id)
+        {
+            try
+            {
+                var country = await _unitofWork.Countries.Get(q => q.Id == id, new List<string> { "Hotels" });
+                var result = _mapper.Map<IList<CountryDTO>>(country);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something Went Wrong in the {nameof(GetCountry)}");
                 return StatusCode(500, "Internal Server Error. Please Try Again Later.");
             }
         }
